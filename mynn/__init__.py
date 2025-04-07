114b5d69161c9ec06228969db99e45c3c5b7427c
LeoRobotic <qwe123.qq11@qq.com>
2025-04-07 11:15:49 +0800

    revise __init__.py

--- mynn/__init__.py
+++ mynn/__init__.py
@@ -1,6 +1,14 @@
-from mynn import op
-from mynn import optimizer
-from mynn import models
-from mynn import lr_scheduler
-from mynn import runner
-from mynn import metric
+# from mynn import op
+# from mynn import optimizer
+# from mynn import models
+# from mynn import lr_scheduler
+# from mynn import runner
+# from mynn import metric
+
+# project-name-independent import pattern
+import .op
+import .optimizer
+import .models
+import .lr_scheduler
+import .runner
+import .metric
