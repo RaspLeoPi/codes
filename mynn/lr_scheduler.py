21a9c5de91554e797213e087a5479e4d233cba37
LeoRobotic <qwe123.qq11@qq.com>
2025-04-09 15:28:35 +0800

    complete the multisteplr layer

--- mynn/lr_scheduler.py
+++ mynn/lr_scheduler.py
@@ -24,7 +24,17 @@ class StepLR(scheduler):
             self.step_count = 0
 
 class MultiStepLR(scheduler):
-    pass
+    def __init__(self, optimizer, step_size=30, gamma=0.1, milestones=None):
+        super().__init__(optimizer)
+        self.step_size = step_size
+        self.gamma = gamma
+        self.milestones = milestones    # a list
+    
+    def step(self) -> None:
+        self.step_count += 1
+        if self.step_count in self.milestones:
+            self.optimizer.init_lr *= self.gamma
+
 
 class ExponentialLR(scheduler):
     pass
