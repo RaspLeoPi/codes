ce9c482c4dbd8237eb629bca3ca973be91f91d8a
LeoRobotic <qwe123.qq11@qq.com>
2025-04-29 15:59:46 +0800

    feat: delete irrelevant code block

--- weight_visualization.py
+++ weight_visualization.py
@@ -6,23 +6,19 @@ import gzip
 import matplotlib.pyplot as plt
 import pickle
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--test", default="1", help="The index of the model you want to visualize")
+parser.add_argument("--model", default="linear", choices=["linear", "conv"], 
+                    help="specify the kind of model to visualize")
+
+args = parser.parse_args()
+
 model = nn.models.Model_MLP()
-model.load_model(r'.\saved_models\best_model_1.pickle')
+model.load_model(f'.\\saved_models\\test{args.test}.pickle')
 
-test_images_path = r'.\dataset\MNIST\t10k-images-idx3-ubyte.gz'
-test_labels_path = r'.\dataset\MNIST\t10k-labels-idx1-ubyte.gz'
-
-with gzip.open(test_images_path, 'rb') as f:
-        magic, num, rows, cols = unpack('>4I', f.read(16))
-        test_imgs=np.frombuffer(f.read(), dtype=np.uint8).reshape(num, 28*28)
-    
-with gzip.open(test_labels_path, 'rb') as f:
-        magic, num = unpack('>2I', f.read(8))
-        test_labs = np.frombuffer(f.read(), dtype=np.uint8)
-
-test_imgs = test_imgs / test_imgs.max()
-
-# logits = model(test_imgs)
+# TODO: complete the visualization for linear layer and conv layer
 
 mats = []
 mats.append(model.layers[0].params['W'])
